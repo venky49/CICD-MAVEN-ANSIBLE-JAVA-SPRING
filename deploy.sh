c5a0e39e98601ccebd464b80b28f462fad164052
koti pasupureddy <koti.pasupureddy@gmail.com>
2018-05-19 10:32:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo ln -s /var/lib/jenkins/workspace/helloworld-tomcat/target ~/deploy
-cd  /root/deploy
+cd  /root/
+cd /deploy/
 sudo mv spring3-mvc-maven-xml-hello-world-1.0-SNAPSHOT.war helloworld.war
 sudo chmod 666 helloworld.war
 sudo scp -r helloworld.war 172.18.1.2:/usr/share/tomcat/webapps/
